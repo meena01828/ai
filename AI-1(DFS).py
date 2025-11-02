3625f0e5169fe762717da28198c3c260e04eddba
meena01828 <minakshikumawat08@gmail.com>
2025-11-02 10:44:50 +0530

    Add files via upload

---
+++ AI-1(DFS).py
@@ -0,0 +1,23 @@
+# 2. DFS Recursive
+
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    print(start, end=" ")
+    visited.add(start)
+    for neighbour in graph[start]:
+        if neighbour not in visited:
+            dfs(graph, neighbour, visited)
+
+# Example graph (Adjacency List)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print("DFS starting from A:")
+dfs(graph, 'B')
