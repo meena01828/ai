ce93078338d0de0aae9cfd59531b58b10065e454
meena01828 <minakshikumawat08@gmail.com>
2025-11-02 10:41:07 +0530

    Add files via upload

---
+++ AI-1(BFS).py
@@ -0,0 +1,23 @@
+from collections import deque
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(neighbour for neighbour in graph[node] if neighbour not in visited)
+
+# Example graph (Adjacency List)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print("BFS starting from A:")
+bfs(graph, 'D')
